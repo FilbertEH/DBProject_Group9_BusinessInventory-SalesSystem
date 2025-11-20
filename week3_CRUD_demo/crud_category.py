1986702c30f165ca3d26d575d58de0859d596170
keitakawabata-ugm <keitakawabata@mail.ugm.ac.id>
2025-11-20 18:04:22 +0700

    🚧 Add CRUD module skeletons (product, customer, category) for team

---
+++ week3_CRUD_demo/crud_category.py
@@ -0,0 +1,10 @@
+from db_connect import get_connection
+
+def create_category(name):
+    # TODO: Implement SQL Insert
+    print("Function create_category() not implemented yet.")
+
+def get_all_categories():
+    # TODO: Implement SQL Select
+    print("Function get_all_categories() not implemented yet.")
+    return [] # Return empty list to prevent main.py crash
